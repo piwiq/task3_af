b13db42348727c76cdb64f2d30f70ab25c437fc6
EC2 Default User <ec2-user@ip-172-31-81-143.ec2.internal>
2023-01-14 15:08:53 +0000

    task3_only_func

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  #get 'users/show'
+  #get 'users/index'
+  #get 'users/edit'
+  #get 'books/new'
+  #get 'books/show'
+  #get 'books/index'
+  #get 'books/edit'
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+
+  resources :books, only:[:new, :index, :show, :edit, :create, :destroy, :update]
+  resources :users, only:[:index, :show, :edit, :update]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
